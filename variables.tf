946e8f0f00adfddf4d07b283dd238953497247d9
froy001 <davidfradis@gmail.com>
2019-06-25 22:41:45 +0300

    first initialized variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 #  Variables.tf declares has the default variables that are shared by all environments
 # $var.region, $var.domain, $var.tf_s3_bucket
 
+
 # Read credentials from environment variables
 #$ export AWS_ACCESS_KEY_ID="anaccesskey"
 #$ export AWS_SECRET_ACCESS_KEY="asecretkey"
@@ -13,7 +14,6 @@ provider "aws" {
 
 data "terraform_remote_state" "master_state" {
   backend = "s3"
-
   config {
     bucket = "${var.tf_s3_bucket}"
     region = "${var.region}"
@@ -25,34 +25,19 @@ variable "aws_profile" {
   description = "Which AWS profile is should be used? Defaults to \"default\""
   default     = "default"
 }
+variable "region" { default = "us-east-2" }
 
-variable "region" {
-  default = "us-east-1"
-}
 
 # This should be changed to reflect the service / stack defined by this repo
 # for example replace "ref" with "cms", "slackbot", etc
-variable "stack" {
-  default = "ref"
-}
+variable "stack" { default = "ref" }
 
 variable "tf_s3_bucket" {
   description = "S3 bucket Terraform can use for state"
-  default     = "rk-devops-state-us-east-1"
+  default     = "tf-patterns-devops-state-us-east-2"
 }
 
-variable "master_state_file" {
-  default = "terraform-api-gateway/state/base/base.tfstate"
-}
-
-variable "prod_state_file" {
-  default = "terraform-api-gateway/state/production/production.tfstate"
-}
-
-variable "staging_state_file" {
-  default = "terraform-api-gateway/state/staging/staging.tfstate"
-}
-
-variable "dev_state_file" {
-  default = "terraform-api-gateway/state/dev/dev.tfstate"
-}
+variable "master_state_file" { default = "terraform-patterns/state/base/base.tfstate" }
+variable "prod_state_file" { default = "terraform-patterns/state/production/production.tfstate" }
+variable "staging_state_file" { default = "terraform-patterns/state/staging/staging.tfstate" }
+variable "dev_state_file" { default = "terraform-patterns/state/dev/dev.tfstate" }
