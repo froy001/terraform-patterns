7da74b72f6500055d8cb1b1fecbbe1e481cc3e27
froy001 <davidfradis@gmail.com>
2019-07-07 13:11:28 +0300

    added my s3 backend module

--- base/s3.tf
+++ base/s3.tf
@@ -1,7 +1,5 @@
-module "tfstate-backend" {
-  source  ="git::https://github.com/froy001/terraform-aws-backend-s3.git"
-  # insert the 1 required variable here
-  # region = "${var.region}"
-  backend_dynamodb_lock_table = "${var.dynamodb_lock_table}"
-  s3_key = "${var.tf_s3_bucket}"
+variable "dynamodb_lock_table" {
+  description = "The name of the dynamodb lock table for the env"
+  type = "string"
+  default = "tf-infrastructure-terraformLockTable"
 }
